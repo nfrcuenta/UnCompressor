df814a57d2f58a55b6d0235370bc7fd66f823aa1
Johan <johanarturobarajas@gmail.com>
2025-11-17 12:25:38 -0500

    intento 1 de main

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <algorithm>
-
+#include "huffman/MatrixHuffman.hpp"
 int main() {
     std::string ruta;
     
@@ -60,6 +60,43 @@ int main() {
     auto columnas = std::get<1>(resultado);
     auto matriz = std::get<2>(resultado);
    
-    // ... mostrar resultados
+    // ==========================================
+    // INTEGRACIÓN DE LA LIBRERÍA HUFFMAN
+    // ==========================================
+
+    std::cout << "\n--- Iniciando Compresion Huffman ---\n";
+
+    // 1. Preparar el valor de fondo (más frecuente)
+ 
+    std::string valorFondoStr = std::to_string(codepoint_mas_frecuente);
+
+    // 2. Convertir la matriz del Normalizer a huffman::Triplete
+    std::vector<huffman::Triplete> entradaHuffman;
+
+    for (const auto& elemento : matriz) {
+
+        int f = std::get<0>(elemento); 
+        int c = std::get<1>(elemento); 
+        auto v = std::get<2>(elemento);
+        std::string valorStr = std::to_string(v);
+        
+        if (valorStr != valorFondoStr) {
+            entradaHuffman.push_back({f, c, valorStr});
+        }
+    }
+    std::string archivoSalida = "matriz_comprimida.txt";
+    
+    auto diccionario = huffman::procesarMatrizYExportar(
+        entradaHuffman,
+        valorFondoStr,
+        filas,
+        columnas,
+        archivoSalida
+    );
+
+    // 4. Mostrar reporte
+    std::cout << "Compresion finalizada.\n";
+    std::cout << "Archivo generado: " << archivoSalida << "\n";
+    std::cout << "Tamaño del diccionario: " << diccionario.size() << " simbolos unicos.\n";
     return 0;
 }
